fb90cfabb3324476ff57ffd860ae774ee039330f
luongngochoa <81282520+luongngochoa@users.noreply.github.com>
2022-03-11 21:45:33 +0700

    Added Database, AutoMapper, Unit of Work and DTOs

---
+++ HotelListing/Configurations/MapperInitilizer.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Configurations
+{
+    public class MapperInitilizer : Profile
+    {
+        public MapperInitilizer()
+        {
+            //fields in both data classes has direct correlation, in both ways.
+            CreateMap<Country, CountryDTO>().ReverseMap();
+            CreateMap<Country, CreateCountryDTO>().ReverseMap();
+            CreateMap<Hotel, HotelDTO>().ReverseMap();
+            CreateMap<Hotel, CreateHotelDTO>().ReverseMap();
+        }
+    }
+}
